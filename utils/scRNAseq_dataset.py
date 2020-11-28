46b31c67ca766dfee86d61bdb9c2aeca9a63c03e
Qi Wu <qiii.wu@utoronto.ca>
2020-11-28 16:53:17 -0500

    first commit

---
+++ utils/scRNAseq_dataset.py
@@ -0,0 +1,10 @@
+# write a sampler to make a meaningful split of training/test dataset.
+    # Make sure that the CellType labels are relatively balanced.
+    # Make sure that the Method annotations are relatively balanced.
+
+# write a dataset object
+
+# write pytorch-lightning DataModule
+
+# write classification pytorch module and lightning module
+
