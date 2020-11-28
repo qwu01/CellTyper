bf0a0883a57365fd393bcc4ecd05cad611096c3b
Qi Wu <qiii.wu@utoronto.ca>
2020-11-28 16:58:24 -0500

    tst

--- utils/scRNAseq_dataset.py
+++ utils/scRNAseq_dataset.py
@@ -1,3 +1,6 @@
+from argparse import ArgumentParser, Namespace
+
+
 # write a sampler to make a meaningful split of training/test dataset.
     # Make sure that the CellType labels are relatively balanced.
     # Make sure that the Method annotations are relatively balanced.
